cc7c77c8f8ee7b82380258be603a975c4d308017
kaveh-mad@ <mad.kaveh@gmail.com>
2019-01-09 13:45:39 +0330

    for  free  game plaing

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1547028257,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/fram-sheet0.png",
+		"images/loder-sheet0.png",
+		"images/spritefontsuper.png",
+		"media/clic-clac-mp4.m4a",
+		"media/clic-clac-mp4.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
